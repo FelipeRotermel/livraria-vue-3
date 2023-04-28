3569a90061bb834ce1044516594ae8113432f1b4
Felipe Rotermel <feliperotermell@gmail.com>
2023-04-28 15:15:57 -0300

    LivrosView quase funcioando

--- src/views/LivrosView.vue
+++ src/views/LivrosView.vue
@@ -1,38 +1,38 @@
 <script>
 import LivrosApi from "@/api/livros";
-import EditorasApi from "@/api/editoras";
 import AutoresApi from "@/api/autores";
-import CatagoriasApi from "@/api/categorias";
+import EditorasApi from "@/api/editoras";
+import CategoriasApi from "@/api/categorias";
 const livrosApi = new LivrosApi();
-const editorasApi = new EditorasApi();
 const autoresApi = new AutoresApi();
-const categoriasApi = new CatagoriasApi();
+const editorasApi = new EditorasApi();
+const categoriasApi = new CategoriasApi();
 export default {
   data() {
-  return {
-    livros : [],
-    livro: {},
-    autor: {},
-    editora: {},
-    categoria: {},
-  };
-},
+    return {
+      livros: [],
+      livro: {},
+      autor: {},
+      editora: {},
+      categoria: {},
+    };
+  },
   async created() {
-  this.livros = await livrosApi.buscarTodosOsLivros();
-  this.autores = await autoresApi.buscarTodosOsAutores();
-  this.editoras = await editorasApi.buscarTodasAsEditoras();
-  this.categorias = await categoriasApi.buscarTodasAsCategorias();
+    this.livros = await livrosApi.buscarTodosOsLivros();
+    this.autores = await autoresApi.buscarTodosOsAutores();
+    this.editoras = await editorasApi.buscarTodasAsEditoras();
+    this.categorias = await categoriasApi.buscarTodasAsCategorias();
   },
   methods: {
     async salvar() {
-  if (this.livro.id) {
-    await livrosApi.atualizarLivro(this.livro);
-  } else {
-    await livrosApi.atualizarLivro(this.livro);
-  }
-  this.livro = {};
-  this.livros = await livrosApi.buscarTodosOsLivros();
-},
+      if (this.livro.id) {
+        await livrosApi.atualizarLivro(this.livro);
+      } else {
+        await livrosApi.adicionarLivro(this.livro);
+      }
+      this.livro = {};
+      this.livros = await livrosApi.buscarTodosOsLivros();
+    },
     editar(livro) {
       Object.assign(this.livro, livro);
     },
@@ -58,13 +58,13 @@ export default {
     <option v-for="autor in autores" :value="autor.id">{{ autor.nome }}</option>
   </select>
 
-  <label for="editora-select">Editora:</label>
-  <select id="editora-select" v-model="livro.editora">
+  <label>Editora:</label>
+  <select v-model="livro.editora">
     <option v-for="editora in editoras" :value="editora.id">{{ editora.nome }}</option>
   </select>
 
-  <label for="categoria-select">Categoria:</label>
-  <select id="categoria-select" v-model="livro.categoria">
+  <label>Categoria:</label>
+  <select v-model="livro.categoria">
     <option v-for="categoria in categorias" :value="categoria.id">{{ categoria.descricao }}</option>
   </select>
 
@@ -73,11 +73,19 @@ export default {
   <hr />
   <ul>
     <li v-for="livro in livros" :key="livro.id">
-      <span @click="editar(livro)">
+      <span class="spanLivro" @click="editar(livro)">
         ({{ livro.id }}) - {{ livro.titulo }} - {{ livro.isbn }} - {{ livro.quantidade }} - {{ livro.preco }}
       </span>
-      <button @click="excluir(livro)">X</button>
+      <button type="button" class="btn btn-danger" @click="excluir(livro)">X</button>
     </li>
   </ul>
 </template>
 
+<style scoped>
+
+.spanLivro {
+  cursor: pointer;
+  font-size: large;
+  margin-right: 10px;
+}
+</style>
