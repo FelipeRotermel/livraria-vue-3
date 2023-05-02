da13812077293e21363b32310c4b26c480079285
Felipe Rotermel <feliperotermell@gmail.com>
2023-05-02 15:09:04 -0300

    commit

--- src/views/LivrosView.vue
+++ src/views/LivrosView.vue
@@ -10,9 +10,11 @@ const categoriasApi = new CategoriasApi();
 export default {
   data() {
     return {
+      categorias: [],
+      editoras: [],
+      autores: [],
       livros: [],
       livro: {},
-      autor: {},
       editora: {},
       categoria: {},
     };
@@ -45,40 +47,44 @@ export default {
 </script>
 
 <template>
-  <h1 class="titulo">Livros</h1>
-  <hr />
-  <div class="form input-group">
-  <input class="form-control" type="text" v-model="livro.titulo" placeholder="Título" />
-  <input class="form-control" type="text" v-model="livro.isbn" placeholder="ISBN" />
-  <input class="form-control" type="text" v-model="livro.quantidade" placeholder="Quantidade" />
-  <input class="form-control" type="text" v-model="livro.preco" placeholder="Preço" />
+  <div class="container-fluid">
+    <h1 class="titulo">Livros</h1>
+    <div class="infoContent">
+      <hr />
+      <div class="form input-group">
+        <input class="form-control" type="text" v-model="livro.titulo" placeholder="Título" />
+        <input class="form-control" type="text" v-model="livro.isbn" placeholder="ISBN" />
+        <input class="form-control" type="text" v-model="livro.quantidade" placeholder="Quantidade" />
+        <input class="form-control" type="text" v-model="livro.preco" placeholder="Preço" />
 
-  <label for="autor-select">Autor:</label>
-  <select id="autor-select" v-model="livro.autor">
-    <option v-for="autor in autores" :value="autor.id">{{ autor.nome }}</option>
-  </select>
+        <label>Autor:</label>
+        <select class="form-select" v-model="livro.autor">
+          <option v-for="autor in autores" :value="autor.id">{{ autor.nome }}</option>
+        </select>
 
-  <label>Editora:</label>
-  <select v-model="livro.editora">
-    <option v-for="editora in editoras" :value="editora.id">{{ editora.nome }}</option>
-  </select>
+        <label>Editora:</label>
+        <select class="form-select" v-model="livro.editora">
+          <option v-for="editora in editoras" :value="editora.id">{{ editora.nome }}</option>
+        </select>
 
-  <label>Categoria:</label>
-  <select v-model="livro.categoria">
-    <option v-for="categoria in categorias" :value="categoria.id">{{ categoria.descricao }}</option>
-  </select>
+        <label>Categoria:</label>
+        <select class="form-select" v-model="livro.categoria">
+          <option v-for="categoria in categorias" :value="categoria.id">{{ categoria.descricao }}</option>
+        </select>
 
-  <button class="btn btn-outline-secondary" @click="salvar">Salvar</button>
-</div>
-  <hr />
-  <ul>
-    <li v-for="livro in livros" :key="livro.id">
-      <span class="spanLivro" @click="editar(livro)">
-        ({{ livro.id }}) - {{ livro.titulo }} - {{ livro.isbn }} - {{ livro.quantidade }} - {{ livro.preco }}
-      </span>
-      <button type="button" class="btn btn-danger" @click="excluir(livro)">X</button>
-    </li>
-  </ul>
+        <button class="btn btn-outline-secondary" @click="salvar">Salvar</button>
+      </div>
+      <hr />
+    </div>
+    <ul>
+      <li v-for="livro in livros" :key="livro.id">
+        <span class="spanLivro" @click="editar(livro)">
+          ({{ livro.id }}) - {{ livro.titulo }} - {{ livro.isbn }} - {{ livro.quantidade }} - {{ livro.preco }}
+        </span>
+        <button type="button" class="btn btn-danger" @click="excluir(livro)">X</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
@@ -87,5 +93,14 @@ export default {
   cursor: pointer;
   font-size: large;
   margin-right: 10px;
+}
+
+.input-group {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
 }
 </style>
